94bbd9f490777364d5e8722d7ed6591099dfade6
pallineczka <paulina.stelmasiak97@gmail.com>
2019-05-14 19:01:14 +0200

    nothing

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,11 +1,37 @@
 package edu.iis.mto.testreactor.exc1;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
-
+import org.junit.Before;
 import org.junit.Test;
 
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class CoffeeMachineTest {
+
+    private CoffeeReceipe receipe;
+    private Grinder grinder;
+    private MilkProvider milkProvider;
+    private CoffeeReceipe receipes;
+    private CoffeeReceipe.Builder builder;
+    private CoffeOrder order;
+    private Coffee coffee;
+    private CoffeeMachine machine;
+
+    @Before
+    public void initialize(){
+
+        coffee = new Coffee();
+
+        receipes = mock(CoffeeReceipe.class);
+        when(receipes.getReceipe(order.getType()).thenReturn(order.getSize().SMALL));
+
+        order = new CoffeeOrder();
+        machine = new CoffeeMachine(CoffeeSize.SMALL, milkProvider.heat(), receipes);
+
+    }
 
     @Test
     public void itCompiles() {
