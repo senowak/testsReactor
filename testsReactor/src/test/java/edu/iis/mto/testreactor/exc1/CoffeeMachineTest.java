20340aa50141eec6a89bc66025fec396f2590fc5
ostrowski.kamil <209418@edu.p.lodz.pl>
2019-05-14 18:37:22 +0200

    Added test for Milk Amount in make method.

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,15 +1,44 @@
 package edu.iis.mto.testreactor.exc1;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 public class CoffeeMachineTest {
+
+    HashMap<CoffeeSize, Integer> map = new HashMap();
+    Grinder grinder;
+    CoffeeReceipes coffeeReceipes;
+    MilkProvider milkProvider;
+
+    @Before public void initialise() {
+        map.put(CoffeeSize.STANDARD, 100);
+        map.put(CoffeeSize.DOUBLE, 200);
+        map.put(CoffeeSize.SMALL, 80);
+        grinder = mock(Grinder.class);
+        coffeeReceipes = mock(CoffeeReceipes.class);
+        milkProvider = mock(MilkProvider.class);
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
+    @Test public void testCoffeMachineMakeMilkAmount() {
+        CoffeeReceipe coffeeReceipe = CoffeeReceipe.builder().withMilkAmount(100).withWaterAmounts(map).build();
+        when(coffeeReceipes.getReceipe(CoffeType.ESPRESSO)).thenReturn(coffeeReceipe);
+        when(grinder.grind(CoffeeSize.STANDARD)).thenReturn(true);
+        CoffeOrder coffeOrder = CoffeOrder.builder().withType(CoffeType.ESPRESSO).withSize(CoffeeSize.STANDARD).build();
+        CoffeeMachine coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+        Coffee result = coffeeMachine.make(coffeOrder);
+        assertThat(result.getMilkAmout(), is(100));
+    }
 }
