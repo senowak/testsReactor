be399b37bf6672e58e98f192544be69c8dbde739
Antoni Urbaniak <202323@edu.p.lodz.pl>
2019-05-14 18:27:00 +0200

    Added new tests: aCoffeeMachineWithGoodToolsTest;/ CoffeOrderBuilderWithSizeAndTypeTest;/ Generated simple classes for grinder, milkprovider and coffeereceipes

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -11,9 +11,11 @@ import org.junit.Test;
 
 public class CoffeeMachineTest {
 
-    Grinder grinder;// = (Grinder) new Object();
-    MilkProvider milkProvider;// = (MilkProvider) new Object();
-    CoffeeReceipes coffeeReceipe;// = (CoffeeReceipe) new Object();
+    Grinder grinder;
+    MilkProvider milkProvider;
+    CoffeeReceipes coffeeReceipes;
+    CoffeeMachine coffeeMachine;
+    CoffeOrder myLittleOrder;
 
     @Test
     public void itCompiles() {
@@ -22,14 +24,64 @@ public class CoffeeMachineTest {
 
     @Test
     public void aCoffeeMachineWithNullTools() {
-        CoffeeMachine coffeeMachine;
         try {
-            coffeeMachine = new CoffeeMachine(grinder, milkProvider,coffeeReceipe);
+            coffeeMachine = new CoffeeMachine(grinder, milkProvider,coffeeReceipes);
             fail();
         }catch(NullPointerException e) {
             //success
         }
     }
 
+    @Test
+    public void aCoffeeMachineWithGoodToolsTest(){
+        grinder = new GenericGrinder();
+        milkProvider = new GenericMilkProvider();
+        coffeeReceipes = new CasualCoffeeReceipes();
+        try {
+            coffeeMachine = new CoffeeMachine(grinder, milkProvider,coffeeReceipes);
+            //success
+        }catch(NullPointerException e) {
+            fail();
+        }
+    }
 
+    @Test
+    public void CoffeOrderBuilderWithSizeAndTypeTest() {
+        myLittleOrder = CoffeOrder.builder().withSize(CoffeeSize.SMALL).withType(CoffeType.ESPRESSO).build();
+        assertThat(myLittleOrder.getType(),is(equalTo(CoffeType.ESPRESSO)));
+        assertThat(myLittleOrder.getSize(),is(equalTo(CoffeeSize.SMALL)));
+    }
+
+//    @Test
+//    public void CoffeeMachineMakeCoffeWithCorrectOrderTest(){
+//
+//    }
+
+    class GenericGrinder implements Grinder {
+
+        @Override public boolean grind(CoffeeSize size) {
+            if(size == CoffeeSize.SMALL)
+                return false;
+            else
+                return true;
+        }
+    }
+
+    class GenericMilkProvider implements MilkProvider {
+
+        @Override public void heat() {
+            System.out.println("Burn baby burn!");
+        }
+
+        @Override public void pour(int milkAmount) {
+            System.out.println("One Milk with Coffee please!");
+        }
+    }
+
+    class CasualCoffeeReceipes implements CoffeeReceipes {
+
+        @Override public CoffeeReceipe getReceipe(CoffeType type) {
+            return null;
+        }
+    }
 }
