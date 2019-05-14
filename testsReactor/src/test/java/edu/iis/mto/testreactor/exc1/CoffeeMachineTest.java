534c4ef6cc18805cdb32d358d1e749ea728ffdbc
209523 <209523@CARBON.CTI.P.LODZ.PL>
2019-05-14 18:01:42 +0200

    initial setup

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,23 +1,34 @@
 package edu.iis.mto.testreactor.exc1;
+import static org.mockito.Mockito.*;
 
 
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class CoffeeMachineTest {
 
+	CoffeeMachine coffeMachineToBeTested;
+	
+    Grinder grinder = Mockito.mock(Grinder.class);
+    MilkProvider milkProvider =  Mockito.mock(MilkProvider.class);
+    CoffeeReceipes receipes = Mockito.mock(CoffeeReceipes.class);
+	
+	@Before
+	public void setUpMethod(){
+		coffeMachineToBeTested = new CoffeeMachine(grinder, milkProvider, receipes);
+		
+		
+	}
+	
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
     
-    @Test
-    public void testIfCreateMethodReturnsCoffe() {
-        Coffee coffee = new Coffee();
-        CoffeeReceipe receipe = receipes.getReceipe(order.getType());
-        
-    }
+
 }
