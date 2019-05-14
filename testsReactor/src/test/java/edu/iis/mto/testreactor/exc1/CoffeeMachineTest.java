38d71625ae248e652f6b98f335a7b78796867672
Natalia Ciosek <n_ciosek@wp.pl>
2019-05-14 19:02:06 +0200

    sypiące się testy

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,24 +19,34 @@ public class CoffeeMachineTest {
     private Coffee coffee;
     private CoffeOrder coffeOrder;
     private CoffeeReceipe coffeeReceipe;
-    private CoffeeReceipes coffeeReceipes = mock(CoffeeReceipes.class);
-    private MilkProvider milkProvider = mock(MilkProvider.class);
-    private Grinder grinder = mock(Grinder.class);
+
+    CoffeeReceipes coffeeReceipes = mock(CoffeeReceipes.class);
+    MilkProvider milkProvider = mock(MilkProvider.class);
+    Grinder grinder = mock(Grinder.class);
 
 
     @Before public void setup(){
         coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+
+        coffeeReceipe = new CoffeeReceipe(CoffeeReceipe.builder());
         coffeeReceipe.getMilkAmount();
         coffeeReceipe.getWaterAmount(CoffeeSize.DOUBLE);
         coffeeReceipe.withMilk();
 
-        coffeOrder.builder().withType(CoffeType.ESPRESSO);
-        coffeOrder.builder().withType(CoffeType.LATTE);
-        coffeOrder.builder().withSize(CoffeeSize.DOUBLE);
+        when(coffeeReceipes.getReceipe(CoffeType.LATTE)).thenReturn(coffeeReceipe);
+
+        coffeOrder.builder().build();
     }
 
     @Test public void itCompiles() {
         assertThat(true, equalTo(true));
+    }
+
+    @Test public void sizeTest(){
+        coffee = coffeeMachine.make(coffeOrder);
+
+        assertEquals(2, coffee.getMilkAmout());
+        assertEquals(1, coffee.getWaterAmount());
     }
 
     @Test public void receipIsNullTest(){
@@ -49,16 +60,16 @@ public class CoffeeMachineTest {
     @Test public void grindDoubleCoffeTest(){
         CoffeeSize doubleCoffe = CoffeeSize.DOUBLE;
         Mockito.doReturn(true).when(grinder).grind(doubleCoffe);
-
         CoffeType coffeType = CoffeType.ESPRESSO;
 
         assertEquals(grinder.grind(doubleCoffe), equals(true));
     }
 
     @Test public void isMilkTest(){
-        CoffeType coffeType = CoffeType.ESPRESSO;
-        coffeeReceipe.getMilkAmount();
-        Coffee coffee = new Coffee();
+        CoffeType coffeType = CoffeType.LATTE;
+        Mockito.doReturn(true).when(milkProvider).pour(2);
+
+        assertEquals(true, coffeType);
     }
 
 }
