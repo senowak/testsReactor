6b8a1bae7616d17afa89c861bc7c51c03229b58a
mkarpacka <209348@edu.p.lodz.pl>
2019-05-14 18:03:11 +0200

    setup created

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -3,13 +3,29 @@ package edu.iis.mto.testreactor.exc1;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CoffeeMachineTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, equalTo(true));
+    CoffeeMachine coffeeMachine;
+    Grinder grinder;
+    MilkProvider milkProvider;
+    CoffeeReceipes coffeeReceipes;
+    Coffee coffee;
+
+    @Before
+    public void setup(){
+        grinder = Mockito.mock(Grinder.class);
+        milkProvider = Mockito.mock(MilkProvider.class);
+        coffeeReceipes = Mockito.mock(CoffeeReceipes.class);
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
     }
+
 
 }
