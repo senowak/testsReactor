16ead22fac9d85c33e5322dd157b8175b5c356d6
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-05-14 18:26:03 +0200

    add test grindCoffeeGrinderGrindShouldBeCalledOnce

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -6,6 +6,10 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
@@ -22,13 +26,25 @@ public class CoffeeMachineTest {
 
     private CoffeeMachine coffeeMachine;
     private CoffeOrder coffeOrder;
+    private CoffeeReceipe coffeeReceipe;
+    private Map<CoffeeSize, Integer> waterAmounts = Collections.emptyMap();
 
     @Before
     public void initialize(){
+        waterAmounts = new HashMap<>();
         coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+        waterAmounts.put(CoffeeSize.STANDARD, 10);
 
         coffeOrder = CoffeOrder.builder().withType(CoffeType.ESPRESSO).withSize(CoffeeSize.STANDARD).build();
+        coffeeReceipe = CoffeeReceipe.builder().withMilkAmount(returnNotImportantMilkAmount()).withWaterAmounts(waterAmounts).build();
 
+        when(coffeeReceipes.getReceipe(CoffeType.ESPRESSO)).thenReturn(coffeeReceipe);
+
+
+    }
+
+    private int returnNotImportantMilkAmount() {
+        return 15;
     }
 
     @Test
@@ -40,6 +56,13 @@ public class CoffeeMachineTest {
     public void grindCoffeeGrinderGrindShouldThrowException() {
         when(grinder.grind(CoffeeSize.STANDARD)).thenReturn(false);
         coffeeMachine.make(coffeOrder);
+    }
+
+    @Test
+    public void grindCoffeeGrinderGrindShouldBeCalledAtLeastOnce() {
+        when(grinder.grind(CoffeeSize.STANDARD)).thenReturn(true);
+        coffeeMachine.make(coffeOrder);
+        verify(grinder, atLeastOnce()).grind(CoffeeSize.STANDARD);
     }
 
 
