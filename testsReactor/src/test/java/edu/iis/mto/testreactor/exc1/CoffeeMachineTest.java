43c17424eae9b41498a0691fe3b8b60d25afc926
struszczak.kamila <209472@edu.p.lodz.pl>
2019-05-14 18:13:37 +0200

    dodanie testu waterAmount()

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -2,14 +2,62 @@ package edu.iis.mto.testreactor.exc1;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
+import org.hamcrest.core.Is;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
-public class CoffeeMachineTest {
+import java.util.HashMap;
+
+@RunWith(MockitoJUnitRunner.class) public class CoffeeMachineTest {
+
+    private CoffeeMachine coffeeMachine;
+    private CoffeOrder coffeOrder;
+    @Mock Grinder grinder;
+    @Mock MilkProvider milkProvider;
+    @Mock CoffeeReceipes coffeeReceipes;
+
+    @Before
+    public void init(){
+
+        HashMap waterAmounts = new HashMap();
+        waterAmounts.put(CoffeeSize.STANDARD, 10);
+
+        CoffeeReceipe.Builder coffeReeceipeBuilder = CoffeeReceipe.builder()
+                                                                  .withWaterAmounts(waterAmounts)
+                                                                  .withMilkAmount(5);
+
+        CoffeOrder.Builder coffeOrderBuilder = CoffeOrder.builder()
+                                                         .withSize(CoffeeSize.STANDARD)
+                                                         .withType(CoffeType.ESPRESSO);
+
+        coffeOrder = coffeOrderBuilder.build();
+        CoffeeReceipe coffeeReceipe = coffeReeceipeBuilder.build();
+
+        when(grinder.grind(any(CoffeeSize.class))).thenReturn(true);
+        when(coffeeReceipes.getReceipe(any(CoffeType.class))).thenReturn(coffeeReceipe);
+
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
+    }
+
+    @Test
+    public void waterAmount(){
+
+        Coffee coffee = coffeeMachine.make(coffeOrder);
+
+        assertThat(coffee.getWaterAmount(), Is.is(10));
+
     }
 
 }
