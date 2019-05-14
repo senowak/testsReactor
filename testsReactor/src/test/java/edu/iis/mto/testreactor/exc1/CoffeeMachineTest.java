a9c0926c9f611807d02075d6e728224bf15be41d
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-05-14 17:57:57 +0200

    mocks and setup added

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -5,8 +5,27 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CoffeeMachineTest {
 
+    @Mock
+    Grinder grinder;
+    @Mock
+    CoffeeReceipes receipes;
+    @Mock
+    MilkProvider milkProvider;
+
+    private Map<CoffeeSize, Integer> coffeeSizes;
+    private CoffeeMachine machine;
+
+    @Before
+    public void setUp() {
+        coffeeSizes = new HashMap<>();
+        coffeeSizes.put(CoffeeSize.SMALL, 1);
+        coffeeSizes.put(CoffeeSize.STANDARD, 2);
+        coffeeSizes.put(CoffeeSize.DOUBLE, 4);
+        machine = new CoffeeMachine(grinder, milkProvider, receipes);
+    }
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
