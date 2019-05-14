1216298a436963cec445f25522a294df18364703
, <202249@10.0.75.1>
2019-05-14 19:00:16 +0200

    1 test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,15 +1,43 @@
 package edu.iis.mto.testreactor.exc1;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
-import org.junit.Test;
+import org.junit.Before;
 
 public class CoffeeMachineTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, equalTo(true));
+    private CoffeeMachine mach;
+    private MilkProvider milkProvider;
+    private CoffeeReceipes coffeeReceipes;
+    private CoffeOrder kawa;
+    private Grinder grinder;
+    private CoffeType coffeType;
+    private CoffeeSize coffeeSize;
+
+    @Before
+    public void init() {
+        coffeType = CoffeType.CAPUCCINO;
+        coffeeSize = CoffeeSize.DOUBLE;
+        kawa = kawa.builder()
+                   .withSize(coffeeSize)
+                   .withType(coffeType)
+                   .build();
+        grinder = mock(Grinder.class);
+        milkProvider = mock(MilkProvider.class);
+        coffeeReceipes = mock(CoffeeReceipes.class);
+        mach = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
     }
 
-}
+    @org.junit.Test(expected = NoCoffeeBeansException.class)
+    public void ZiarenNieMaWmaszynie() {
+        Coffee kawas = mach.make(kawa);
+    }
+
+    /*
+     * @Test public void asdasd() { Coffee kawa = mach.make(any(CoffeOrder.class)); verify(grinder,
+     * times(0)).grind(any(CoffeeSize.class));
+     *
+     * }
+     */
+
+};
