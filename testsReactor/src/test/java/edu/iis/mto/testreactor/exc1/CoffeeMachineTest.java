6d6d5d5a601e36d50f74d6385f13838dc5ea6603
Maksymilian Reguła <maksymilian.regula@gmail.com>
2019-05-14 17:49:26 +0200

    added mockito dependency

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -3,9 +3,27 @@ package edu.iis.mto.testreactor.exc1;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class CoffeeMachineTest {
+    @Mock
+    private Grinder grinder;
+
+    @Mock
+    private MilkProvider milkProvider;
+
+    @Mock
+    private CoffeeMachine coffeeMachine;
+
+    @Before
+    public void initialize(){
+
+    }
 
     @Test
     public void itCompiles() {
