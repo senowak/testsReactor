73a8f3b6609168c36119344dae194a38be9d1975
wsobczyk <wiktoriasobczyk@gmail.com>
2019-05-14 18:08:41 +0200

    should throw no cofee beans exception test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -2,10 +2,44 @@ package edu.iis.mto.testreactor.exc1;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-public class CoffeeMachineTest {
+@RunWith(MockitoJUnitRunner.class)public class CoffeeMachineTest {
+    Coffee coffee;
+    CoffeOrder coffeOrder;
+    CoffeeMachine coffeeMachine;
+
+
+    @Mock
+    CoffeeReceipes coffeeReceipes;
+
+    @Mock
+    Grinder grinder;
+
+    @Mock
+    MilkProvider milkProvider;
+
+    @Before public void setup() {
+        coffee = new Coffee();
+        coffeeMachine = new CoffeeMachine(grinder,milkProvider,coffeeReceipes);
+    }
+
+
+    @Test(expected = NoCoffeeBeansException.class)
+
+    public void shouldThrowNoCoffeeBeansExceptionWhenThereIsNoCoffee(){
+         coffeOrder = CoffeOrder.builder().withSize(CoffeeSize.SMALL).withType(CoffeType.ESPRESSO)
+                                          .build();
+        Mockito.when(grinder.grind(CoffeeSize.SMALL)).thenReturn(false);
+        coffeeMachine.make(coffeOrder);
+    }
 
     @Test
     public void itCompiles() {
@@ -13,3 +47,4 @@ public class CoffeeMachineTest {
     }
 
 }
+
