9caa1eeff69aa3ea1021b09fc18e8bea520978f9
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-05-14 18:10:54 +0200

    mock interfaces and grindCoffeeGrinderGrindShouldBeCalledOnce test added

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,15 +1,45 @@
 package edu.iis.mto.testreactor.exc1;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
-import org.junit.Test;
-
+@RunWith(MockitoJUnitRunner.class)
 public class CoffeeMachineTest {
+
+    @Mock
+    private Grinder grinder;
+    @Mock
+    private MilkProvider milkProvider;
+    @Mock
+    private CoffeeReceipes coffeeReceipes;
+
+    private CoffeeMachine coffeeMachine;
+    private CoffeOrder coffeOrder;
+
+    @Before
+    public void initialize(){
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+
+        coffeOrder = CoffeOrder.builder().withType(CoffeType.ESPRESSO).withSize(CoffeeSize.STANDARD).build();
+
+    }
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
+    @Test(expected = NoCoffeeBeansException.class)
+    public void grindCoffeeGrinderGrindShouldBeCalledOnce() {
+        when(grinder.grind(CoffeeSize.STANDARD)).thenReturn(false);
+        coffeeMachine.make(coffeOrder);
+        verify(grinder, atLeastOnce()).grind(CoffeeSize.STANDARD);
+    }
 }
