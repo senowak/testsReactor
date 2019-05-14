21754f2a766e8fb3ad2c503f11098e19bc64681d
Grzegorz Kabziński <grzesiek.kab@gmail.com>
2019-05-14 18:31:51 +0200

    Add init and test for coffe order

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,15 +1,53 @@
 package edu.iis.mto.testreactor.exc1;
 
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Collections;
+import java.util.Map;
+
+@RunWith(MockitoJUnitRunner.class)
 public class CoffeeMachineTest {
 
-    @Test
-    public void itCompiles() {
-        assertThat(true, equalTo(true));
+    @Mock
+    private Grinder grinder;
+    @Mock
+    private MilkProvider milkProvider;
+    @Mock
+    private CoffeeReceipes coffeeReceipes;
+
+    private CoffeeMachine coffeeMachine;
+    private CoffeeReceipe coffeeReceipe;
+    private CoffeeReceipe.Builder builder;
+    private CoffeOrder coffeOrder;
+
+    @Before
+    public void init(){
+        grinder = mock(Grinder.class);
+        milkProvider = mock(MilkProvider.class);
+        coffeeReceipes = mock(CoffeeReceipes.class);
+        coffeeMachine = new CoffeeMachine(grinder,milkProvider,coffeeReceipes);
+        coffeOrder = CoffeOrder.builder().withSize(CoffeeSize.SMALL).withType(CoffeType.CAPUCCINO).build();
+
     }
+
+    @Test
+    public void testIfCoffeTypeAndSizeIsCorrect(){
+        Assert.assertEquals(coffeOrder.getType(),CoffeType.CAPUCCINO);
+        Assert.assertEquals(coffeOrder.getSize(),CoffeeSize.SMALL);
+    }
+
 
 }
