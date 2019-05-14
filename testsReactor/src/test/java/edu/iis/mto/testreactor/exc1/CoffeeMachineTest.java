f9d45b9ae9e1f2c8b014857d3b171c808dd10393
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-05-14 17:58:10 +0200

    test for valid order added

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,9 +1,19 @@
 package edu.iis.mto.testreactor.exc1;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CoffeeMachineTest {
@@ -26,9 +36,25 @@ public class CoffeeMachineTest {
         coffeeSizes.put(CoffeeSize.DOUBLE, 4);
         machine = new CoffeeMachine(grinder, milkProvider, receipes);
     }
+
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
+    @Test
+    public void OrderForStandardCoffeeShouldReturnStandardCoffee() {
+        when(grinder.grind(CoffeeSize.STANDARD)).thenReturn(true);
+        CoffeeReceipe receipe = CoffeeReceipe.builder()
+                                             .withMilkAmount(1)
+                                             .withWaterAmounts(this.coffeeSizes)
+                                             .build();
+        when(receipes.getReceipe(CoffeType.CAPUCCINO)).thenReturn(receipe);
+
+        CoffeOrder order = CoffeOrder.builder().withSize(CoffeeSize.STANDARD).withType(CoffeType.CAPUCCINO).build();
+        Coffee result = machine.make(order);
+
+        Assert.assertThat(result.getMilkAmout(), is(1));
+        Assert.assertThat(result.getWaterAmount(), is(2));
+    }
 }
