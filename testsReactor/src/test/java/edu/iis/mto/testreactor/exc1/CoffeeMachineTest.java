a89b6c4083664b08c3ff75a6d4fdc864789e651a
wsobczyk <wiktoriasobczyk@gmail.com>
2019-05-14 18:14:55 +0200

    added shouldReturnCoffeeWithTheSameAmountOfWaterAsInCoffeeReceipt test

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.testreactor.exc1;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -11,38 +12,46 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)public class CoffeeMachineTest {
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(MockitoJUnitRunner.class) public class CoffeeMachineTest {
+
     Coffee coffee;
     CoffeOrder coffeOrder;
     CoffeeMachine coffeeMachine;
+    CoffeeReceipe coffeeReceipe;
 
+    @Mock CoffeeReceipes coffeeReceipes;
 
-    @Mock
-    CoffeeReceipes coffeeReceipes;
+    @Mock Grinder grinder;
 
-    @Mock
-    Grinder grinder;
-
-    @Mock
-    MilkProvider milkProvider;
+    @Mock MilkProvider milkProvider;
 
     @Before public void setup() {
         coffee = new Coffee();
-        coffeeMachine = new CoffeeMachine(grinder,milkProvider,coffeeReceipes);
+        coffeeMachine = new CoffeeMachine(grinder, milkProvider, coffeeReceipes);
+        coffeOrder = CoffeOrder.builder().withSize(CoffeeSize.SMALL).withType(CoffeType.ESPRESSO).build();
     }
-
 
     @Test(expected = NoCoffeeBeansException.class)
 
-    public void shouldThrowNoCoffeeBeansExceptionWhenThereIsNoCoffee(){
-         coffeOrder = CoffeOrder.builder().withSize(CoffeeSize.SMALL).withType(CoffeType.ESPRESSO)
-                                          .build();
-        Mockito.when(grinder.grind(CoffeeSize.SMALL)).thenReturn(false);
+    public void shouldThrowNoCoffeeBeansExceptionWhenThereIsNoCoffee() {
+        when(grinder.grind(CoffeeSize.SMALL)).thenReturn(false);
         coffeeMachine.make(coffeOrder);
     }
 
-    @Test
-    public void itCompiles() {
+    @Test public void shouldReturnCoffeeWithTheSameAmountOfWaterAsInCoffeeReceipt() {
+        Map<CoffeeSize, Integer> coffeeMap = new HashMap<>();
+        coffeeMap.put(CoffeeSize.SMALL, 30);
+        coffeeReceipe = CoffeeReceipe.builder().withMilkAmount(15).withWaterAmounts(coffeeMap).build();
+        when(grinder.grind(CoffeeSize.SMALL)).thenReturn(true);
+        when(coffeeReceipes.getReceipe(CoffeType.ESPRESSO)).thenReturn(coffeeReceipe);
+        Coffee coffee = coffeeMachine.make(coffeOrder);
+        assertThat(coffeeReceipe.getWaterAmount(CoffeeSize.SMALL), is(coffee.getWaterAmount()));
+    }
+
+    @Test public void itCompiles() {
         assertThat(true, equalTo(true));
     }
 
