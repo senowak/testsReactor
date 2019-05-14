292e2fc0b9b07f1c2f2f1b3a397b8a70bee5d668
Antoni Urbaniak <202323@edu.p.lodz.pl>
2019-05-14 17:59:47 +0200

    Added new test: aCoffeeMAchineWithNullTools;

--- testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc1/CoffeeMachineTest.java
@@ -1,15 +1,35 @@
 package edu.iis.mto.testreactor.exc1;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CoffeeMachineTest {
+
+    Grinder grinder;// = (Grinder) new Object();
+    MilkProvider milkProvider;// = (MilkProvider) new Object();
+    CoffeeReceipes coffeeReceipe;// = (CoffeeReceipe) new Object();
 
     @Test
     public void itCompiles() {
         assertThat(true, equalTo(true));
     }
+
+    @Test
+    public void aCoffeeMachineWithNullTools() {
+        CoffeeMachine coffeeMachine;
+        try {
+            coffeeMachine = new CoffeeMachine(grinder, milkProvider,coffeeReceipe);
+            fail();
+        }catch(NullPointerException e) {
+            //success
+        }
+    }
+
 
 }
