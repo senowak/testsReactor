8a1d51d33c3e7d9cfd8056217c4801785672b1ee
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2019-05-15 15:41:10 +0200

    test stub added

---
+++ testsReactor/src/test/java/edu/iis/mto/testreactor/exc2/WashingMachineTest.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.testreactor.exc2;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class WashingMachineTest {
+
+    @Test
+    public void itCompiles() {
+        assertThat(true, Matchers.equalTo(true));
+    }
+
+}
